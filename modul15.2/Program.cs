b2453cbf319cb3097d508990bb72b553b6a2fedb
AuyeskhanM <auyeskhanm@gmail.com>
2019-04-10 22:47:29 +0600

    Добавьте файлы проекта.

---
+++ modul15.2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul15._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type console = typeof(Console);
+            MethodInfo[] methods = console.GetMethods();
+            Console.WriteLine("Список методов класса Console: \n");
+            foreach (MethodInfo m in methods)
+            {
+                Console.Write(m.ReturnType.Name + "\t" + m.Name);
+                Console.Write("\n");
+            }
+            Console.ReadLine();
+        }
+    }
+}
